7bcc9178ed85a02656eae2671de2765e4fc7e48b
Carlos Panato <ctadeu@gmail.com>
2019-10-14 16:15:51 +0200

    add some logs

--- server/circleci.go
+++ server/circleci.go
@@ -16,10 +16,12 @@ import (
 func (s *Server) triggerCircleCiIfNeeded(pr *model.PullRequest) {
 	client := &circleci.Client{Token: s.Config.CircleCIToken}
 
+	mlog.Info("Checking if need trigger circleci", mlog.String("repo", pr.RepoName), mlog.Int("pr", pr.Number), mlog.String("fullname", pr.FullName))
 	repoInfo := strings.Split(pr.FullName, "/")
 	if repoInfo[0] == "mattermost" {
 		// It is from upstream mattermost repo dont need to trigger the circleci because org members
 		// have permissions
+		mlog.Info("Dont need to trigger circleci", mlog.String("repo", pr.RepoName), mlog.Int("pr", pr.Number), mlog.String("fullname", pr.FullName))
 		return
 	}
 
@@ -44,4 +46,5 @@ func (s *Server) triggerCircleCiIfNeeded(pr *model.PullRequest) {
 	if err != nil {
 		mlog.Error("Error triggering circleci", mlog.String("repo", pr.RepoName), mlog.Int("pr", pr.Number), mlog.String("Fullname", pr.FullName))
 	}
+	mlog.Info("Triggered circleci", mlog.String("repo", pr.RepoName), mlog.Int("pr", pr.Number), mlog.String("fullname", pr.FullName))
 }
