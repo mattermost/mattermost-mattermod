dc067dab8f2730836d775d36b98e7efd75ded118
Carlos Panato <ctadeu@gmail.com>
2019-10-14 17:04:39 +0200

    fix missing parameter

--- server/circleci.go
+++ server/circleci.go
@@ -26,7 +26,7 @@ func (s *Server) triggerCircleCiIfNeeded(pr *model.PullRequest) {
 	}
 
 	// Checking if the repo have circleci setup
-	builds, err := client.ListRecentBuildsForProject(pr.RepoOwner, pr.RepoName, "master", "", 5, 0)
+	builds, err := client.ListRecentBuildsForProject("github", pr.RepoOwner, pr.RepoName, "master", "", 5, 0)
 	if err != nil {
 		mlog.Error("listing the circleci project", mlog.String("repo", pr.RepoName), mlog.Int("pr", pr.Number), mlog.String("Fullname", pr.FullName))
 		return
