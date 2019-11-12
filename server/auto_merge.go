4696a83bb0691e80243baced5dde9664ff6c988d
Carlos Panato <ctadeu@gmail.com>
2019-11-12 08:44:53 +0100

    update per review

--- server/auto_merge.go
+++ server/auto_merge.go
@@ -30,7 +30,7 @@ func (s *Server) AutoMergePR() {
 
 		prToMerge, _, err := client.PullRequests.Get(context.Background(), pr.RepoOwner, pr.RepoName, pr.Number)
 		if err != nil {
-			mlog.Error("Error to gettinh the PR info", mlog.Int("pr", pr.Number), mlog.String("repo", pr.RepoName), mlog.Err(err))
+			mlog.Error("Error in getting the PR info", mlog.Int("pr", pr.Number), mlog.String("repo", pr.RepoName), mlog.Err(err))
 			continue
 		}
 
@@ -39,18 +39,24 @@ func (s *Server) AutoMergePR() {
 		}
 
 		if prToMerge.GetMergeableState() != "clean" {
-			mlog.Info("PR is not ready to merge", mlog.Int("pr", pr.Number), mlog.String("repo", pr.RepoName))
+			mlog.Info("PR is not ready to merge, does not have a good merge state", mlog.Int("pr", pr.Number), mlog.String("repo", pr.RepoName), mlog.String("mergeableState", prToMerge.GetMergeableState()))
 			continue
 		}
 
 		// Get the Statuses
 		PRStatus, _, err := client.Repositories.GetCombinedStatus(context.Background(), pr.RepoOwner, pr.RepoName, prToMerge.Head.GetSHA(), nil)
 		if err != nil {
-			mlog.Error("Error to gettinh the PR Status", mlog.Int("pr", pr.Number), mlog.String("repo", pr.RepoName), mlog.Err(err))
+			mlog.Error("Error in getting the PR Status", mlog.Int("pr", pr.Number), mlog.String("repo", pr.RepoName), mlog.Err(err))
 			continue
 		}
-		if prToMerge.Head.GetSHA() != PRStatus.GetSHA() && PRStatus.GetState() != "success" {
-			mlog.Info("PR is not ready to merge", mlog.Int("pr", pr.Number), mlog.String("repo", pr.RepoName))
+
+		if prToMerge.Head.GetSHA() != PRStatus.GetSHA() {
+			mlog.Error("PR is not ready to merge, missmatch SHA", mlog.Int("pr", pr.Number), mlog.String("repo", pr.RepoName), mlog.String("SHAFromPR", prToMerge.Head.GetSHA()), mlog.String("SHAFromStatus", PRStatus.GetSHA()))
+			continue
+		}
+
+		if PRStatus.GetState() != "success" {
+			mlog.Error("PR is not ready to merge, not ready state", mlog.Int("pr", pr.Number), mlog.String("repo", pr.RepoName), mlog.String("state", PRStatus.GetState()))
 			continue
 		}
 
@@ -74,9 +80,11 @@ func (s *Server) AutoMergePR() {
 			SHA:         prToMerge.Head.GetSHA(),
 			MergeMethod: "merge",
 		}
+
 		merged, _, err := client.PullRequests.Merge(context.Background(), pr.RepoOwner, pr.RepoName, pr.Number, "Automatic Merge", opt)
 		if err != nil {
 			errMsg := fmt.Sprintf("Error while trying to automerge the PR\nErr %s", err.Error())
+			s.removeLabel(pr.RepoOwner, pr.RepoName, pr.Number, s.Config.AutoPRMergeLabel)
 			s.sendGitHubComment(pr.RepoOwner, pr.RepoName, pr.Number, errMsg)
 			continue
 		}
@@ -87,13 +95,9 @@ func (s *Server) AutoMergePR() {
 
 }
 
-func (s *Server) isAutoMergeLabel(label string) bool {
-	return label == s.Config.AutoPRMergeLabel
-}
-
 func (s *Server) isAutoMergeLabelInLabels(labels []string) bool {
 	for _, label := range labels {
-		if s.isAutoMergeLabel(label) {
+		if label == s.Config.AutoPRMergeLabel {
 			return true
 		}
 	}
