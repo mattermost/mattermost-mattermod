650c743db3d4c10ca25ba4373b2f9bba42482e72
Elisabeth Kulzer <elikul@elikul.de>
2020-02-12 22:36:20 +0100

    Fix style

--- server/build_mobile_apps.go
+++ server/build_mobile_apps.go
@@ -13,11 +13,12 @@ import (
 
 func (s *Server) buildMobileApp(pr *model.PullRequest) {
 	prRepoOwner, prRepoName, prNumber := pr.RepoOwner, pr.RepoName, pr.Number
-	ref := "refs/heads/"+s.Config.BuildMobileAppBranchPrefix + strconv.Itoa(prNumber)
+	ref := "refs/heads/" + s.Config.BuildMobileAppBranchPrefix + strconv.Itoa(prNumber)
 
 	isReadyToBeBuilt, err := s.areChecksSuccessfulForPr(pr)
 	if err != nil {
-		s.sendGitHubComment(prRepoOwner, prRepoName, prNumber,"Failed to retrieve the status of the PR. Error:  \n```"+err.Error()+"```",)
+		s.sendGitHubComment(prRepoOwner, prRepoName, prNumber,
+			"Failed to retrieve the status of the PR. Error:  \n```"+err.Error()+"```")
 		return
 	}
 
@@ -26,13 +27,14 @@ func (s *Server) buildMobileApp(pr *model.PullRequest) {
 		if exists {
 			err := s.deleteRef(s.Config.Username, prRepoName, ref)
 			if err != nil {
-				s.sendGitHubComment(prRepoOwner, prRepoName, prNumber,"Failed to delete already existing build branch. @mattermost/core-build-engineers have been notified. Error:  \n```"+err.Error()+"```",)
+				s.sendGitHubComment(prRepoOwner, prRepoName, prNumber,
+					"Failed to delete already existing build branch. @mattermost/core-build-engineers have been notified. Error:  \n```"+err.Error()+"```")
 				return
 			}
 		}
 
 		s.createRef(pr, ref)
-		s.sendGitHubComment(prRepoOwner, prRepoName, prNumber, s.Config.BuildMobileAppInitMessage,)
+		s.sendGitHubComment(prRepoOwner, prRepoName, prNumber, s.Config.BuildMobileAppInitMessage)
 
 		ctx, cancel := context.WithTimeout(context.Background(), 45*time.Minute)
 		defer cancel()
@@ -40,20 +42,23 @@ func (s *Server) buildMobileApp(pr *model.PullRequest) {
 		buildLink, buildNumber, err := s.waitForBuildLink(ctx, pr, s.Config.Username)
 		if err != nil {
 			mlog.Err(err)
-			s.sendGitHubComment(prRepoOwner, prRepoName, prNumber,"Failed retrieving build link. @mattermost/core-build-engineers have been notified. Error:  \n```"+err.Error()+"```",)
+			s.sendGitHubComment(prRepoOwner, prRepoName, prNumber,
+				"Failed retrieving build link. @mattermost/core-build-engineers have been notified. Error:  \n```"+err.Error()+"```")
 			return
 		}
-		s.sendGitHubComment(prRepoOwner, prRepoName, prNumber, "Successfully building: " + buildLink,)
+		s.sendGitHubComment(prRepoOwner, prRepoName, prNumber, "Successfully building: "+buildLink)
 
 		artifactLinks, err := s.waitForArtifactLinks(ctx, pr, s.Config.Username, buildNumber)
 		if err != nil {
-			s.sendGitHubComment(prRepoOwner, prRepoName, prNumber,"Failed retrieving artifact links. @mattermost/core-build-engineers have been notified. Error:  \n```"+err.Error()+"```",)
+			s.sendGitHubComment(prRepoOwner, prRepoName, prNumber,
+				"Failed retrieving artifact links. @mattermost/core-build-engineers have been notified. Error:  \n```"+err.Error()+"```")
 			return
 		}
-		s.sendGitHubComment(prRepoOwner, prRepoName, prNumber, "Artifact links: " + artifactLinks,)
+		s.sendGitHubComment(prRepoOwner, prRepoName, prNumber, "Artifact links: "+artifactLinks)
 
-		_ = s.deleteRefWhereCombinedStateEqualsSuccess(s.Config.Username, prRepoName, ref,)
+		_ = s.deleteRefWhereCombinedStateEqualsSuccess(s.Config.Username, prRepoName, ref)
 	} else {
-		s.sendGitHubComment(prRepoOwner, prRepoName, prNumber,"Not triggering the mobile app build workflow, because PR checks are failing. ",)
+		s.sendGitHubComment(prRepoOwner, prRepoName, prNumber,
+			"Not triggering the mobile app build workflow, because PR checks are failing. ")
 	}
 }
