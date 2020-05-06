df65a0f61a44a23f5cafd027ae96db6762950a92
Ben Schumacher <ben.schumacher@mattermost.com>
2020-05-06 20:18:51 +0200

    Fix merge

--- server/github_client.go
+++ server/github_client.go
@@ -52,6 +52,7 @@ type PullRequestsService interface {
 type RepositoriesService interface {
 	CreateStatus(ctx context.Context, owner, repo, ref string, status *github.RepoStatus) (*github.RepoStatus, *github.Response, error)
 	Get(ctx context.Context, owner, repo string) (*github.Repository, *github.Response, error)
+	GetBranch(ctx context.Context, owner, repo, branch string) (*github.Branch, *github.Response, error)
 	GetCombinedStatus(ctx context.Context, owner, repo, ref string, opts *github.ListOptions) (*github.CombinedStatus, *github.Response, error)
 	ListTeams(ctx context.Context, owner string, repo string, opts *github.ListOptions) ([]*github.Team, *github.Response, error)
 	ListStatuses(ctx context.Context, owner, repo, ref string, opts *github.ListOptions) ([]*github.RepoStatus, *github.Response, error)
