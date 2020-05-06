da32219f86ec841b4c313c30f7e198a31ad9a112
Elisabeth Kulzer <elikul@elikul.de>
2020-05-06 13:33:04 +0200

    Remove old jenkins status. Refactor finding upstream and forked branch logic. Find same named ee branch.

--- server/enterprise.go
+++ server/enterprise.go
@@ -53,19 +53,20 @@ func (s *Server) triggerEnterpriseTests(pr *model.PullRequest) {
 
 type EETriggerInfo struct {
 	BaseBranch   string
+	EEBranch     string
 	ServerOwner  string
 	ServerBranch string
 	WebappOwner  string
 	WebappBranch string
 }
 
-func (s *Server) getPRInfo(ctx context.Context, pr *model.PullRequest) (*EETriggerInfo, error) {
+func (s *Server) getPRInfo(ctx context.Context, pr *model.PullRequest) (info *EETriggerInfo, err error) {
 	clientGitHub := NewGithubClient(s.Config.GithubAccessToken)
 	pullRequest, _, err := clientGitHub.PullRequests.Get(ctx, s.Config.Org, pr.RepoName, pr.Number)
 	if err != nil {
 		return nil, err
 	}
-
+	baseBranch := pullRequest.Base.GetRef()
 	isFork := pullRequest.GetHead().GetRepo().GetFork()
 	var serverOwner string
 	if isFork {
@@ -77,12 +78,26 @@ func (s *Server) getPRInfo(ctx context.Context, pr *model.PullRequest) (*EETrigg
 		return nil, fmt.Errorf("owner of server branch not found")
 	}
 
-	webappOwner, webappBranch, err := s.findWebappBranch(clientGitHub, ctx, pullRequest)
+	eeBranch, err := s.getBranchWithSameName(ctx, s.Config.Org, s.Config.EnterpriseReponame, pr.Ref)
 	if err != nil {
 		return nil, err
 	}
-	info := &EETriggerInfo{
-		BaseBranch:   *pullRequest.Base.Ref,
+	if eeBranch == "" {
+		eeBranch = baseBranch
+	}
+
+	webappOwner, webappBranch, err := s.getSameBranchInRepo(ctx, pr, s.Config.EnterpriseWebappReponame)
+	if err != nil {
+		return nil, err
+	}
+	if webappBranch == "" {
+		webappOwner = s.Config.Org
+		webappBranch = baseBranch
+	}
+
+	info = &EETriggerInfo{
+		BaseBranch:   baseBranch,
+		EEBranch:     eeBranch,
 		ServerOwner:  serverOwner,
 		ServerBranch: pr.Ref,
 		WebappOwner:  webappOwner,
@@ -91,54 +106,40 @@ func (s *Server) getPRInfo(ctx context.Context, pr *model.PullRequest) (*EETrigg
 	return info, nil
 }
 
-func (s *Server) findWebappBranch(client *github.Client, ctx context.Context, serverPR *github.PullRequest) (string, string, error) {
-	serverBranch := serverPR.GetHead().GetRef()
-	destinationServerBranch := serverPR.GetBase().GetRef()
+func (s *Server) getBranchWithSameName(ctx context.Context, remote string, repo string, ref string) (branch string, err error) {
+	clientGitHub := NewGithubClient(s.Config.GithubAccessToken)
+	ghBranch, r, err := clientGitHub.Repositories.GetBranch(ctx, remote, repo, ref)
+	if err != nil {
+		if r == nil {
+			return "", err
+		}
+		if r.StatusCode != http.StatusNotFound {
+			return "", err
+		}
+		return "", nil // do not err if branch is not found
+	}
+	if ghBranch == nil {
+		return "", fmt.Errorf("unexpected failure case")
+	}
+	return ghBranch.GetName(), nil
+}
 
-	owner, webappBranch, err := s.getWebappBranchWithSameName(client, ctx, serverPR)
+func (s *Server) getSameBranchInRepo(ctx context.Context, serverPR *model.PullRequest, repo string) (owner string, branch string, err error) {
+	forkBranch, err := s.getBranchWithSameName(ctx, serverPR.Username, repo, serverPR.Ref)
 	if err != nil {
 		return "", "", err
 	}
-
-	if webappBranch == "" {
-		mlog.Debug("Setting base ref of server as webapp branch", mlog.Int("pr", serverPR.GetNumber()), mlog.String("ref", serverBranch))
-		return s.Config.Org, destinationServerBranch, nil
-	}
-
-	return owner, webappBranch, nil
-}
-
-func (s *Server) getWebappBranchWithSameName(client *github.Client, ctx context.Context, serverPR *github.PullRequest) (owner string, branch string, err error) {
-	prAuthor := serverPR.GetUser().GetLogin()
-	ref := serverPR.GetHead().GetRef()
-	forkBranch, r, err := client.Repositories.GetBranch(ctx, prAuthor, s.Config.EnterpriseWebappReponame, ref)
-	if err != nil {
-		if r == nil {
-			return "", "", err
-		}
-
-		if r.StatusCode != http.StatusNotFound {
-			return "", "", err
-		}
-
-		upstreamBranch, r, err := client.Repositories.GetBranch(ctx, s.Config.Org, s.Config.EnterpriseWebappReponame, ref)
+	if forkBranch == "" {
+		upstreamBranch, err := s.getBranchWithSameName(ctx, s.Config.Org, repo, serverPR.Ref)
 		if err != nil {
-			if r == nil {
-				return "", "", err
-			}
-
-			if r.StatusCode != http.StatusNotFound {
-				return "", "", err
-			}
-			return s.Config.Org, serverPR.GetBase().GetRef(), nil
+			return "", "", err
 		}
-
-		owner := s.Config.Org
-		mlog.Debug("Found upstream webapp branch", mlog.String("owner", owner), mlog.String("branch", upstreamBranch.GetName()))
-		return owner, upstreamBranch.GetName(), nil
+		if upstreamBranch == forkBranch {
+			return s.Config.Org, "", nil
+		}
+		return s.Config.Org, upstreamBranch, nil
 	}
-	mlog.Debug("Found webapp branch", mlog.String("owner", prAuthor), mlog.String("branch", forkBranch.GetName()))
-	return prAuthor, forkBranch.GetName(), nil
+	return serverPR.Username, forkBranch, nil
 }
 
 func (s *Server) createEnterpriseTestsPendingStatus(ctx context.Context, pr *model.PullRequest) {
@@ -171,16 +172,6 @@ func (s *Server) createEnterpriseTestsErrorStatus(ctx context.Context, pr *model
 	s.createRepoStatus(ctx, pr, enterpriseErrorStatus)
 	s.sendGitHubComment(pr.RepoOwner, pr.RepoName, pr.Number,
 		"Failed running enterprise tests. @mattermost/core-build-engineers have been notified. Error:  \n```"+err.Error()+"```")
-}
-
-func (s *Server) succeedOutDatedJenkinsStatuses(ctx context.Context, pr *model.PullRequest) {
-	enterpriseStatus := &github.RepoStatus{
-		State:       github.String("success"),
-		Context:     github.String("continuous-integration/jenkins/pr-merge"),
-		Description: github.String("Outdated check"),
-		TargetURL:   github.String(""),
-	}
-	s.createRepoStatus(ctx, pr, enterpriseStatus)
 }
 
 func (s *Server) succeedEEStatuses(ctx context.Context, pr *model.PullRequest, desc string) {
