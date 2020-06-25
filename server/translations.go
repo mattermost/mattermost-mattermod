ad34752feb62b00a199f5932f591a6648de97423
Elisabeth Kulzer <elikul@elikul.de>
2020-06-25 12:48:47 +0200

    Use string for time.

--- server/translations.go
+++ server/translations.go
@@ -13,7 +13,7 @@ func (s *Server) handleTranslationPr(pr *model.PullRequest) {
 		return
 	}
 
-	dataMsg := fmt.Sprintf("####[%v translations PR %v](%v)\n", pr.RepoName, time.Now().UTC(), pr.URL)
+	dataMsg := fmt.Sprintf("####[%v translations PR %v](%v)\n", pr.RepoName, time.Now().UTC().Format(time.RFC3339), pr.URL)
 	msg := dataMsg + s.Config.TranslationsMattermostMessage
 	mlog.Debug("Sending Mattermost message", mlog.String("message", msg))
 
