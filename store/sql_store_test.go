af4011e94fc9b51878ce9914a09aafbe4a370f14
Mario de Frutos <mario@defrutos.org>
2020-07-08 21:30:40 +0200

    Default root username is root

--- store/sql_store_test.go
+++ store/sql_store_test.go
@@ -13,7 +13,7 @@ import (
 const (
 	defaultMysqlDSN     = "mattermod:mattermod@tcp(localhost:3306)/mattermost_mattermod_test?charset=utf8mb4,utf8\u0026readTimeout=30s\u0026writeTimeout=30s\u0026parseTime=true"
 	defaultMysqlUser    = "mattermod"
-	defaultMysqlRootPWD = "mattermod"
+	defaultMysqlRootPWD = "root"
 	defaultMysqlUserPWD = "mattermod"
 	defaultMysqlDB      = "mattermod_test"
 )
