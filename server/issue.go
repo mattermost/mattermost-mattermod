a7aeff3fa8499e2bccb929307b4e82fba0ee96e3
Ibrahim Serdar Acikgoz <serdaracikgoz86@gmail.com>
2020-07-17 09:10:37 +0300

    server: add some error propogation to handleIssueEvent

--- server/issue.go
+++ server/issue.go
@@ -5,47 +5,41 @@ package server
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"strings"
 
 	"github.com/mattermost/mattermost-mattermod/model"
 	"github.com/mattermost/mattermost-server/v5/mlog"
 )
 
-func (s *Server) handleIssueEvent(ctx context.Context, event *PullRequestEvent) {
+func (s *Server) handleIssueEvent(ctx context.Context, event *PullRequestEvent) error {
 	if event == nil || event.Issue == nil {
-		return
+		return errors.New("could not handle issue event: either event or issue is nil")
 	}
 	parts := strings.Split(*event.Issue.HTMLURL, "/")
 
 	mlog.Info("handle issue event", mlog.String("repoUrl", *event.Issue.HTMLURL), mlog.String("Action", event.Action), mlog.Int("PRNumber", event.PRNumber))
 	issue, err := s.GetIssueFromGithub(ctx, parts[len(parts)-4], parts[len(parts)-3], event.Issue)
 	if err != nil {
-		mlog.Error("Error getting the issue from Github", mlog.Err(err))
-		return
+		return fmt.Errorf("error getting the issue from GitHub: %w", err)
 	}
 
-	s.checkIssueForChanges(ctx, issue)
+	return s.checkIssueForChanges(ctx, issue)
 }
 
-func (s *Server) checkIssueForChanges(ctx context.Context, issue *model.Issue) {
+func (s *Server) checkIssueForChanges(ctx context.Context, issue *model.Issue) error {
 	oldIssue, err := s.Store.Issue().Get(issue.RepoOwner, issue.RepoName, issue.Number)
 	if err != nil {
-		mlog.Error(err.Error())
-		return
+		return err
 	}
 
 	if oldIssue == nil {
-		if _, err := s.Store.Issue().Save(issue); err != nil {
-			mlog.Error(err.Error())
-		}
-		return
+		_, err = s.Store.Issue().Save(issue)
+		return err
 	}
 
-	hasChanges := false
-
-	if oldIssue.State != issue.State {
-		hasChanges = true
-	}
+	hasChanges := oldIssue.State != issue.State
 
 	for _, label := range issue.Labels {
 		hadLabel := false
@@ -59,30 +53,31 @@ func (s *Server) checkIssueForChanges(ctx context.Context, issue *model.Issue) {
 
 		if !hadLabel {
 			mlog.Info("issue added label", mlog.Int("issue", issue.Number), mlog.String("label", label))
-			s.handleIssueLabeled(ctx, issue, label)
+			err = s.handleIssueLabeled(ctx, issue, label)
+			if err != nil {
+				return fmt.Errorf("could not handle issue label added: %w", err)
+			}
 			hasChanges = true
 		}
 	}
 
 	if hasChanges {
 		mlog.Info("issue has changes", mlog.Int("issue", issue.Number))
-
-		if _, err := s.Store.Issue().Save(issue); err != nil {
-			mlog.Error(err.Error())
-			return
-		}
+		_, err = s.Store.Issue().Save(issue)
+		return err
 	}
+
+	return nil
 }
 
-func (s *Server) handleIssueLabeled(ctx context.Context, issue *model.Issue, addedLabel string) {
+func (s *Server) handleIssueLabeled(ctx context.Context, issue *model.Issue, addedLabel string) error {
 	// Must be sure the comment is created before we let anouther request test
 	s.commentLock.Lock()
 	defer s.commentLock.Unlock()
 
 	comments, _, err := s.GithubClient.Issues.ListComments(ctx, issue.RepoOwner, issue.RepoName, issue.Number, nil)
 	if err != nil {
-		mlog.Error("issue_error", mlog.Err(err))
-		return
+		return fmt.Errorf("could not get issue from GitHub: %w", err)
 	}
 
 	for _, label := range s.Config.IssueLabels {
@@ -92,4 +87,5 @@ func (s *Server) handleIssueLabeled(ctx context.Context, issue *model.Issue, add
 			s.sendGitHubComment(ctx, issue.RepoOwner, issue.RepoName, issue.Number, finalMessage)
 		}
 	}
+	return nil
 }
