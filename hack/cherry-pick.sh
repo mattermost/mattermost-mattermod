56aba0e7c08d51c0b16f65297c6658948d87b6dd
Ashish Bhate <bhate.ashish@gmail.com>
2020-07-22 17:29:32 +0530

    move function declarations to the top

--- hack/cherry-pick.sh
+++ hack/cherry-pick.sh
@@ -28,78 +28,6 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-REPO_ROOT="$(git rev-parse --show-toplevel)"
-declare -r REPO_ROOT
-cd "${REPO_ROOT}"
-
-STARTINGBRANCH=$(git symbolic-ref --short HEAD)
-declare -r STARTINGBRANCH
-declare -r REBASEMAGIC="${REPO_ROOT}/.git/rebase-apply"
-DRY_RUN=${DRY_RUN:-""}
-REGENERATE_DOCS=${REGENERATE_DOCS:-""}
-UPSTREAM_REMOTE=${UPSTREAM_REMOTE:-upstream}
-FORK_REMOTE=${FORK_REMOTE:-origin}
-MAIN_REPO_ORG=${MAIN_REPO_ORG:-$(git remote get-url "$UPSTREAM_REMOTE" | awk '{gsub(/http[s]:\/\/|git@/,"")}1' | awk -F'[@:./]' 'NR==1{print $3}')}
-MAIN_REPO_NAME=${MAIN_REPO_NAME:-$(git remote get-url "$UPSTREAM_REMOTE" | awk '{gsub(/http[s]:\/\/|git@/,"")}1' | awk -F'[@:./]' 'NR==1{print $4}')}
-
-if [[ -z ${GITHUB_USER:-} ]]; then
-  echo "Please export GITHUB_USER=<your-user> (or GH organization, if that's where your fork lives)"
-  exit 1
-fi
-
-if ! which hub > /dev/null; then
-  echo "Can't find 'hub' tool in PATH, please install from https://github.com/github/hub"
-  exit 1
-fi
-
-if [[ "$#" -lt 2 ]]; then
-  echo "${0} <remote branch> <pr-number> <commit-sha>: cherry pick a <commit> from <pr> onto <remote branch> and leave instructions for proposing pull request"
-  echo
-  echo "  Checks out <remote branch> and handles the cherry-pick of <commits> for you."
-  echo "  Examples:"
-  echo "    $0 upstream/release-3.14 12345 df243fd # Cherry-picks commit sha df243fd onto upstream/release-3.14 and proposes that as a PR with a commit message referring to PR 12345"
-  echo
-  echo "  Set the DRY_RUN environment var to skip git push and creating PR."
-  echo "  This is useful for creating patches to a release branch without making a PR."
-  echo "  When DRY_RUN is set the script will leave you in a branch containing the commits you cherry-picked."
-  echo
-  echo " Set UPSTREAM_REMOTE (default: upstream) and FORK_REMOTE (default: origin)"
-  echo " To override the default remote names to what you have locally."
-  exit 2
-fi
-
-if git_status=$(git status --porcelain --untracked=no 2>/dev/null) && [[ -n "${git_status}" ]]; then
-  echo "!!! Dirty tree. Clean up and try again."
-  exit 1
-fi
-
-while unmerged=$(git status --porcelain | grep ^U) && [[ -n ${unmerged} ]] || [[ -e "${REBASEMAGIC}" ]]; do
-  echo
-  echo "!!! 'git cherry-pick' or other operation in progress. Clean up and try again."
-  exit 1
-done
-
-declare -r BRANCH="$1"
-declare -r PULL="$2"
-declare -r COMMITSHA="$3"
-
-echo "+++ Updating remotes..."
-git remote update "${UPSTREAM_REMOTE}" "${FORK_REMOTE}"
-echo "+++ Updating remotes done..."
-if ! git log -n1 --format=%H "${BRANCH}" >/dev/null 2>&1; then
-  echo "!!! '${BRANCH}' not found. The second argument should be something like ${UPSTREAM_REMOTE}/release-0.21."
-  echo "    (In particular, it needs to be a valid, existing remote branch that I can 'git checkout'.)"
-  exit 1
-fi
-
-NEWBRANCHREQ="automated-cherry-pick-of-#${PULL}" # "Required" portion for tools.
-declare -r NEWBRANCHREQ
-NEWBRANCH="$(echo "${NEWBRANCHREQ}-${BRANCH}" | sed 's/\//-/g')"
-declare -r NEWBRANCH
-NEWBRANCHUNIQ="${NEWBRANCH}-$(date +%s)"
-declare -r NEWBRANCHUNIQ
-echo "+++ Creating local branch ${NEWBRANCHUNIQ}"
-
 cleanbranch=""
 prtext=""
 gitcleanup=false
@@ -146,6 +74,78 @@ EOF
 
 hub pull-request -F "${prtext}" -h "${GITHUB_USER}:${NEWBRANCH}" -b "${MAIN_REPO_ORG}:${rel}"
 }
+
+REPO_ROOT="$(git rev-parse --show-toplevel)"
+declare -r REPO_ROOT
+cd "${REPO_ROOT}"
+
+STARTINGBRANCH=$(git symbolic-ref --short HEAD)
+declare -r STARTINGBRANCH
+declare -r REBASEMAGIC="${REPO_ROOT}/.git/rebase-apply"
+DRY_RUN=${DRY_RUN:-""}
+REGENERATE_DOCS=${REGENERATE_DOCS:-""}
+UPSTREAM_REMOTE=${UPSTREAM_REMOTE:-upstream}
+FORK_REMOTE=${FORK_REMOTE:-origin}
+MAIN_REPO_ORG=${MAIN_REPO_ORG:-$(git remote get-url "$UPSTREAM_REMOTE" | awk '{gsub(/http[s]:\/\/|git@/,"")}1' | awk -F'[@:./]' 'NR==1{print $3}')}
+MAIN_REPO_NAME=${MAIN_REPO_NAME:-$(git remote get-url "$UPSTREAM_REMOTE" | awk '{gsub(/http[s]:\/\/|git@/,"")}1' | awk -F'[@:./]' 'NR==1{print $4}')}
+
+if [[ -z ${GITHUB_USER:-} ]]; then
+  echo "Please export GITHUB_USER=<your-user> (or GH organization, if that's where your fork lives)"
+  exit 1
+fi
+
+if ! which hub > /dev/null; then
+  echo "Can't find 'hub' tool in PATH, please install from https://github.com/github/hub"
+  exit 1
+fi
+
+if [[ "$#" -lt 2 ]]; then
+  echo "${0} <remote branch> <pr-number> <commit-sha>: cherry pick <commit-sha> from <pr> onto <remote branch> and leave instructions for proposing pull request"
+  echo
+  echo "  Checks out <remote branch> and handles the cherry-pick of <commit> for you."
+  echo "  Examples:"
+  echo "    $0 upstream/release-3.14 12345 df243fd # Cherry-picks commit sha df243fd onto upstream/release-3.14 and proposes that as a PR with a commit message referring to PR 12345"
+  echo
+  echo "  Set the DRY_RUN environment var to skip git push and creating PR."
+  echo "  This is useful for creating patches to a release branch without making a PR."
+  echo "  When DRY_RUN is set the script will leave you in a branch containing the commits you cherry-picked."
+  echo
+  echo " Set UPSTREAM_REMOTE (default: upstream) and FORK_REMOTE (default: origin)"
+  echo " To override the default remote names to what you have locally."
+  exit 2
+fi
+
+if git_status=$(git status --porcelain --untracked=no 2>/dev/null) && [[ -n "${git_status}" ]]; then
+  echo "!!! Dirty tree. Clean up and try again."
+  exit 1
+fi
+
+while unmerged=$(git status --porcelain | grep ^U) && [[ -n ${unmerged} ]] || [[ -e "${REBASEMAGIC}" ]]; do
+  echo
+  echo "!!! 'git cherry-pick' or other operation in progress. Clean up and try again."
+  exit 1
+done
+
+declare -r BRANCH="$1"
+declare -r PULL="$2"
+declare -r COMMITSHA="$3"
+
+echo "+++ Updating remotes..."
+git remote update "${UPSTREAM_REMOTE}" "${FORK_REMOTE}"
+echo "+++ Updating remotes done..."
+if ! git log -n1 --format=%H "${BRANCH}" >/dev/null 2>&1; then
+  echo "!!! '${BRANCH}' not found. The second argument should be something like ${UPSTREAM_REMOTE}/release-0.21."
+  echo "    (In particular, it needs to be a valid, existing remote branch that I can 'git checkout'.)"
+  exit 1
+fi
+
+NEWBRANCHREQ="automated-cherry-pick-of-#${PULL}" # "Required" portion for tools.
+declare -r NEWBRANCHREQ
+NEWBRANCH="$(echo "${NEWBRANCHREQ}-${BRANCH}" | sed 's/\//-/g')"
+declare -r NEWBRANCH
+NEWBRANCHUNIQ="${NEWBRANCH}-$(date +%s)"
+declare -r NEWBRANCHUNIQ
+echo "+++ Creating local branch ${NEWBRANCHUNIQ}"
 
 git checkout -b "${NEWBRANCHUNIQ}" "${BRANCH}"
 cleanbranch="${NEWBRANCHUNIQ}"
