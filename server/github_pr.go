mattermost/mattermost-mattermod

commit d174d5f6a3123d8a1e7f2d90239f0ddf3c2cc8d7

[MM-27017] server: add new endpoint for issues webhook (#186)
* server: add new endpoint for issues webhook

* server/issue_handler_test: fix test name

* server/issue: reflect review comments

server/github_pr.go
@@ -5,6 +5,7 @@ package server
 
 import (
 	"encoding/json"
+	"errors"
 	"io"
 	"strings"
 
@@ -60,14 +61,18 @@ func (d *EventData) HasUpdateBranch() bool {
 	return strings.Contains(strings.TrimSpace(*d.Comment.Body), "/update-branch")
 }
 
-func PullRequestEventFromJSON(data io.Reader) *PullRequestEvent {
+func PullRequestEventFromJSON(data io.Reader) (*PullRequestEvent, error) {
 	decoder := json.NewDecoder(data)
 	var event PullRequestEvent
 	if err := decoder.Decode(&event); err != nil {
-		return nil
+		return nil, err
 	}
 
-	return &event
+	if event.Issue == nil {
+		return nil, errors.New("event issue is missing from body")
+	}
+
+	return &event, nil
 }
 
 func EventDataFromJSON(data io.Reader) *EventData {

server/issue_handler.go
@@ -1,0 +1,130 @@
+// Copyright (c) 2017-present Mattermost, Inc. All Rights Reserved.
+// See License.txt for license information.
+
+package server
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/google/go-github/v32/github"
+	"github.com/mattermost/mattermost-mattermod/model"
+	"github.com/mattermost/mattermost-server/v5/mlog"
+	"github.com/pkg/errors"
+)
+
+type issueEvent struct {
+	Action string        `json:"action"`
+	Issue  *github.Issue `json:"issue"`
+}
+
+func (s *Server) issueEventHandler(w http.ResponseWriter, r *http.Request) {
+	ctx, cancel := context.WithTimeout(context.Background(), defaultRequestTimeout*time.Second)
+	defer cancel()
+
+	event, err := issueEventFromJSON(r.Body)
+	if err != nil {
+		mlog.Error("could not parse issue event", mlog.Err(err))
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	mlog.Info("handle issue event",
+		mlog.String("repoUrl", event.Issue.GetHTMLURL()),
+		mlog.String("Action", event.Action),
+		mlog.Int("Issue number", event.Issue.GetNumber()))
+
+	issue, err := s.GetIssueFromGithub(ctx, event.Issue)
+	if err != nil {
+		mlog.Error("could not get the issue from GitHub", mlog.Err(err))
+		http.Error(w, "could not get the issue from GitHub", http.StatusInternalServerError)
+		return
+	}
+
+	if err := s.checkIssueForChanges(ctx, issue); err != nil {
+		mlog.Error("could not check issue for changes", mlog.Err(err))
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func (s *Server) checkIssueForChanges(ctx context.Context, issue *model.Issue) error {
+	oldIssue, err := s.Store.Issue().Get(issue.RepoOwner, issue.RepoName, issue.Number)
+	if err != nil {
+		return err
+	}
+
+	if oldIssue == nil {
+		// TODO: since there is no old entity, we are simply saving the issue and
+		// returning here. However, this logic should be reviewed: MM-27307
+		_, err = s.Store.Issue().Save(issue)
+		return err
+	}
+
+	hasChanges := oldIssue.State != issue.State
+
+	for _, label := range issue.Labels {
+		hadLabel := false
+
+		for _, oldLabel := range oldIssue.Labels {
+			if label == oldLabel {
+				hadLabel = true
+				break
+			}
+		}
+
+		if !hadLabel {
+			mlog.Info("issue added label", mlog.Int("issue", issue.Number), mlog.String("label", label))
+			if err = s.handleIssueLabeled(ctx, issue, label); err != nil {
+				return fmt.Errorf("could not handle issue label added: %w", err)
+			}
+			hasChanges = true
+		}
+	}
+
+	if hasChanges {
+		mlog.Info("issue has changes", mlog.Int("issue", issue.Number))
+		_, err = s.Store.Issue().Save(issue)
+		return err
+	}
+
+	return nil
+}
+
+func (s *Server) handleIssueLabeled(ctx context.Context, issue *model.Issue, addedLabel string) error {
+	// Must be sure the comment is created before we let another request test MM-27284
+	s.commentLock.Lock()
+	defer s.commentLock.Unlock()
+
+	comments, _, err := s.GithubClient.Issues.ListComments(ctx, issue.RepoOwner, issue.RepoName, issue.Number, nil)
+	if err != nil {
+		return fmt.Errorf("could not get issue from GitHub: %w", err)
+	}
+
+	for _, label := range s.Config.IssueLabels {
+		finalMessage := strings.Replace(label.Message, "USERNAME", issue.Username, -1)
+		if label.Label == addedLabel && !messageByUserContains(comments, s.Config.Username, finalMessage) {
+			mlog.Info("Posted message for label on PR", mlog.String("label", label.Label), mlog.Int("issue", issue.Number))
+			s.sendGitHubComment(ctx, issue.RepoOwner, issue.RepoName, issue.Number, finalMessage)
+		}
+	}
+	return nil
+}
+
+func issueEventFromJSON(data io.Reader) (*issueEvent, error) {
+	decoder := json.NewDecoder(data)
+	var event issueEvent
+	if err := decoder.Decode(&event); err != nil {
+		return nil, err
+	}
+
+	if event.Issue == nil {
+		return nil, errors.New("github issue is missing from body")
+	}
+
+	return &event, nil
+}

server/server.go
@@ -91,6 +91,10 @@ func New(config *Config, metrics MetricsProvider) (*Server, error) {
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", s.ping).Methods(http.MethodGet)
+
+	webhooks := r.PathPrefix("/webhooks").Subrouter()
+	webhooks.HandleFunc("/issue", s.issueEventHandler).Methods(http.MethodPost)
+
 	r.HandleFunc("/healthz", s.ping).Methods(http.MethodGet)
 	r.HandleFunc("/pr_event", s.githubEvent).Methods(http.MethodPost)
 	r.Use(s.withRecovery)
@@ -227,7 +231,7 @@ func (s *Server) Tick() {
 				continue
 			}
 
-			issue, err := s.GetIssueFromGithub(ctx, repository.Owner, repository.Name, ghIssue)
+			issue, err := s.GetIssueFromGithub(ctx, ghIssue)
 			if err != nil {
 				mlog.Error("failed to convert issue", mlog.Int("issue", *ghIssue.Number), mlog.Err(err))
 				s.Metrics.IncreaseCronTaskErrors("tick")
@@ -269,8 +273,9 @@ func (s *Server) githubEvent(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	event := PullRequestEventFromJSON(ioutil.NopCloser(bytes.NewBuffer(buf)))
-	if event != nil && event.PRNumber != 0 {
+	// TODO: remove this after migration complete; MM-27283
+	event, err := PullRequestEventFromJSON(ioutil.NopCloser(bytes.NewBuffer(buf)))
+	if err != nil || event.PRNumber != 0 {
 		mlog.Info("pr event", mlog.Int("pr", event.PRNumber), mlog.String("action", event.Action))
 		s.handlePullRequestEvent(ctx, event)
 		return

server/github.go
@@ -87,13 +87,13 @@ func (s *Server) GetPullRequestFromGithub(ctx context.Context, pullRequest *gith
 	return pr, nil
 }
 
-func (s *Server) GetIssueFromGithub(ctx context.Context, repoOwner, repoName string, ghIssue *github.Issue) (*model.Issue, error) {
+func (s *Server) GetIssueFromGithub(ctx context.Context, ghIssue *github.Issue) (*model.Issue, error) {
 	issue := &model.Issue{
-		RepoOwner: repoOwner,
-		RepoName:  repoName,
-		Number:    *ghIssue.Number,
-		Username:  *ghIssue.User.Login,
-		State:     *ghIssue.State,
+		RepoOwner: ghIssue.GetRepository().GetOwner().GetLogin(),
+		RepoName:  ghIssue.GetRepository().GetName(),
+		Number:    ghIssue.GetNumber(),
+		Username:  ghIssue.GetUser().GetLogin(),
+		State:     ghIssue.GetState(),
 	}
 
 	labels, _, err := s.GithubClient.Issues.ListLabelsByIssue(ctx, issue.RepoOwner, issue.RepoName, issue.Number, nil)

server/issue.go
@@ -7,9 +7,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"strings"
 
-	"github.com/mattermost/mattermost-mattermod/model"
 	"github.com/mattermost/mattermost-server/v5/mlog"
 )
 
@@ -17,74 +15,12 @@ func (s *Server) handleIssueEvent(ctx context.Context, event *PullRequestEvent) 
 	if event == nil || event.Issue == nil {
 		return errors.New("could not handle issue event: either event or issue is nil")
 	}
-	parts := strings.Split(*event.Issue.HTMLURL, "/")
 
 	mlog.Info("handle issue event", mlog.String("repoUrl", *event.Issue.HTMLURL), mlog.String("Action", event.Action), mlog.Int("PRNumber", event.PRNumber))
-	issue, err := s.GetIssueFromGithub(ctx, parts[len(parts)-4], parts[len(parts)-3], event.Issue)
+	issue, err := s.GetIssueFromGithub(ctx, event.Issue)
 	if err != nil {
 		return fmt.Errorf("error getting the issue from GitHub: %w", err)
 	}
 
 	return s.checkIssueForChanges(ctx, issue)
-}
-
-func (s *Server) checkIssueForChanges(ctx context.Context, issue *model.Issue) error {
-	oldIssue, err := s.Store.Issue().Get(issue.RepoOwner, issue.RepoName, issue.Number)
-	if err != nil {
-		return err
-	}
-
-	if oldIssue == nil {
-		_, err = s.Store.Issue().Save(issue)
-		return err
-	}
-
-	hasChanges := oldIssue.State != issue.State
-
-	for _, label := range issue.Labels {
-		hadLabel := false
-
-		for _, oldLabel := range oldIssue.Labels {
-			if label == oldLabel {
-				hadLabel = true
-				break
-			}
-		}
-
-		if !hadLabel {
-			mlog.Info("issue added label", mlog.Int("issue", issue.Number), mlog.String("label", label))
-			if err = s.handleIssueLabeled(ctx, issue, label); err != nil {
-				return fmt.Errorf("could not handle issue label added: %w", err)
-			}
-			hasChanges = true
-		}
-	}
-
-	if hasChanges {
-		mlog.Info("issue has changes", mlog.Int("issue", issue.Number))
-		_, err = s.Store.Issue().Save(issue)
-		return err
-	}
-
-	return nil
-}
-
-func (s *Server) handleIssueLabeled(ctx context.Context, issue *model.Issue, addedLabel string) error {
-	// Must be sure the comment is created before we let anouther request test
-	s.commentLock.Lock()
-	defer s.commentLock.Unlock()
-
-	comments, _, err := s.GithubClient.Issues.ListComments(ctx, issue.RepoOwner, issue.RepoName, issue.Number, nil)
-	if err != nil {
-		return fmt.Errorf("could not get issue from GitHub: %w", err)
-	}
-
-	for _, label := range s.Config.IssueLabels {
-		finalMessage := strings.Replace(label.Message, "USERNAME", issue.Username, -1)
-		if label.Label == addedLabel && !messageByUserContains(comments, s.Config.Username, finalMessage) {
-			mlog.Info("Posted message for label on PR", mlog.String("label", label.Label), mlog.Int("issue", issue.Number))
-			s.sendGitHubComment(ctx, issue.RepoOwner, issue.RepoName, issue.Number, finalMessage)
-		}
-	}
-	return nil
 }

server/issue_handler_test.go
@@ -1,0 +1,215 @@
+// Copyright (c) 2017-present Mattermost, Inc. All Rights Reserved.
+// See License.txt for license information.
+
+package server
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"testing"
+
+	"github.com/mattermost/mattermost-mattermod/model"
+	"github.com/mattermost/mattermost-mattermod/server/mocks"
+	stmock "github.com/mattermost/mattermost-mattermod/store/mocks"
+
+	"github.com/golang/mock/gomock"
+	"github.com/google/go-github/v32/github"
+	"github.com/stretchr/testify/require"
+)
+
+func TestIssueEventHandler(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	s := &Server{
+		GithubClient: &GithubClient{},
+		Config: &Config{
+			IssueLabels: []LabelResponse{
+				{
+					Label:   "label-1",
+					Message: "some-message",
+				},
+			},
+			Username: "mattermost",
+		},
+	}
+	is := mocks.NewMockIssuesService(ctrl)
+	s.GithubClient.Issues = is
+
+	isStoreMock := stmock.NewMockIssueStore(ctrl)
+	ss := stmock.NewMockStore(ctrl)
+	ss.EXPECT().
+		Issue().
+		Return(isStoreMock).
+		AnyTimes()
+
+	s.Store = ss
+
+	url := "https://api.github.com/repos/mattermost/mmctl/issues/1"
+	number := 1
+	state := "state"
+	login := "mattermost"
+	name := "mmctl"
+
+	event := issueEvent{
+		Issue: &github.Issue{
+			Repository: &github.Repository{
+				Owner: &github.User{
+					Login: &login,
+				},
+				Name: &name,
+			},
+			Number:  &number,
+			HTMLURL: &url,
+			State:   &state,
+			User: &github.User{
+				Login: &login,
+			},
+		},
+	}
+
+	issue := &model.Issue{
+		RepoOwner: login,
+		RepoName:  name,
+		Username:  login,
+		State:     state,
+		Labels:    []string{},
+		Number:    1,
+	}
+
+	ts := httptest.NewServer(http.HandlerFunc(s.issueEventHandler))
+	defer ts.Close()
+
+	ctxInterface := reflect.TypeOf((*context.Context)(nil)).Elem()
+
+	t.Run("Should fail with no body", func(t *testing.T) {
+		req, err := http.NewRequest("POST", ts.URL, nil)
+		require.NoError(t, err)
+		resp, err := http.DefaultClient.Do(req)
+		require.NoError(t, err)
+		defer resp.Body.Close()
+		require.Equal(t, http.StatusBadRequest, resp.StatusCode)
+	})
+
+	t.Run("Should fail for not getting issue from github", func(t *testing.T) {
+		is.EXPECT().ListLabelsByIssue(gomock.AssignableToTypeOf(ctxInterface), issue.RepoOwner, issue.RepoName, issue.Number, nil).
+			Times(1).
+			Return(nil, nil, errors.New("error"))
+
+		b, err := json.Marshal(&event)
+		require.NoError(t, err)
+
+		req, err := http.NewRequest("POST", ts.URL, bytes.NewReader(b))
+		require.NoError(t, err)
+		resp, err := http.DefaultClient.Do(req)
+		require.NoError(t, err)
+		defer resp.Body.Close()
+		require.Equal(t, http.StatusInternalServerError, resp.StatusCode)
+	})
+
+	t.Run("Issue does not have changes", func(t *testing.T) {
+		is.EXPECT().ListLabelsByIssue(gomock.AssignableToTypeOf(ctxInterface), issue.RepoOwner, issue.RepoName, issue.Number, nil).
+			Times(1).
+			Return([]*github.Label{}, nil, nil)
+
+		isStoreMock.EXPECT().
+			Get(issue.RepoOwner, issue.RepoName, issue.Number).
+			Return(issue, nil).
+			Times(1)
+
+		b, err := json.Marshal(&event)
+		require.NoError(t, err)
+
+		req, err := http.NewRequest("POST", ts.URL, bytes.NewReader(b))
+		require.NoError(t, err)
+		resp, err := http.DefaultClient.Do(req)
+		require.NoError(t, err)
+		defer resp.Body.Close()
+		require.Equal(t, http.StatusOK, resp.StatusCode)
+	})
+
+	t.Run("Issue has changes", func(t *testing.T) {
+		is.EXPECT().ListLabelsByIssue(gomock.AssignableToTypeOf(ctxInterface), issue.RepoOwner, issue.RepoName, issue.Number, nil).
+			Times(1).
+			Return([]*github.Label{}, nil, nil)
+
+		oldIssue := *issue
+		oldIssue.Labels = []string{"labeled"}
+		oldIssue.State = "old-state"
+
+		isStoreMock.EXPECT().
+			Get(oldIssue.RepoOwner, oldIssue.RepoName, oldIssue.Number).
+			Return(&oldIssue, nil).
+			Times(1)
+
+		isStoreMock.EXPECT().
+			Save(issue).
+			Return(nil, nil).
+			AnyTimes()
+
+		b, err := json.Marshal(&event)
+		require.NoError(t, err)
+
+		req, err := http.NewRequest("POST", ts.URL, bytes.NewReader(b))
+		require.NoError(t, err)
+		resp, err := http.DefaultClient.Do(req)
+		require.NoError(t, err)
+		defer resp.Body.Close()
+		require.Equal(t, http.StatusOK, resp.StatusCode)
+	})
+
+	t.Run("Issue labeled", func(t *testing.T) {
+		label := "label-1"
+		body := "some-message"
+		login := "somebody"
+		is.EXPECT().ListLabelsByIssue(gomock.AssignableToTypeOf(ctxInterface), issue.RepoOwner, issue.RepoName, issue.Number, nil).
+			Times(1).
+			Return([]*github.Label{
+				{
+					Name: &label,
+				},
+			}, nil, nil)
+
+		is.EXPECT().ListComments(gomock.AssignableToTypeOf(ctxInterface), issue.RepoOwner, issue.RepoName, issue.Number, nil).
+			Times(1).Return(
+			[]*github.IssueComment{
+				{
+					Body: &body,
+					User: &github.User{ //somebody else is commented
+						Login: &login,
+					},
+				},
+			}, nil, nil)
+
+		is.EXPECT().CreateComment(gomock.AssignableToTypeOf(ctxInterface), issue.RepoOwner, issue.RepoName, issue.Number, &github.IssueComment{Body: &body}).
+			Times(1).Return(nil, nil, nil)
+
+		newIssue := *issue
+		newIssue.Labels = []string{label}
+
+		isStoreMock.EXPECT().
+			Get(issue.RepoOwner, issue.RepoName, issue.Number).
+			Return(issue, nil).
+			Times(1)
+
+		isStoreMock.EXPECT().
+			Save(&newIssue).
+			Return(nil, nil).
+			AnyTimes()
+
+		b, err := json.Marshal(&event)
+		require.NoError(t, err)
+
+		req, err := http.NewRequest("POST", ts.URL, bytes.NewReader(b))
+		require.NoError(t, err)
+		resp, err := http.DefaultClient.Do(req)
+		require.NoError(t, err)
+		defer resp.Body.Close()
+		require.Equal(t, http.StatusOK, resp.StatusCode)
+	})
+}

commit 984eb34a5e7ca83ad8202eb0f9ceea38bd7185b3

server: fix a regression introduced in #186

server/github_pr.go
@@ -5,7 +5,6 @@ package server
 
 import (
 	"encoding/json"
-	"errors"
 	"io"
 	"strings"
 
@@ -61,18 +60,14 @@ func (d *EventData) HasUpdateBranch() bool {
 	return strings.Contains(strings.TrimSpace(*d.Comment.Body), "/update-branch")
 }
 
-func PullRequestEventFromJSON(data io.Reader) (*PullRequestEvent, error) {
+func PullRequestEventFromJSON(data io.Reader) *PullRequestEvent {
 	decoder := json.NewDecoder(data)
 	var event PullRequestEvent
 	if err := decoder.Decode(&event); err != nil {
-		return nil, err
+		return nil
 	}
 
-	if event.Issue == nil {
-		return nil, errors.New("event issue is missing from body")
-	}
-
-	return &event, nil
+	return &event
 }
 
 func EventDataFromJSON(data io.Reader) *EventData {

server/issue_handler.go
@@ -33,6 +33,7 @@ func (s *Server) issueEventHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
+	defer r.Body.Close()
 
 	mlog.Info("handle issue event",
 		mlog.String("repoUrl", event.Issue.GetHTMLURL()),

server/server.go
@@ -274,8 +274,8 @@ func (s *Server) githubEvent(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// TODO: remove this after migration complete; MM-27283
-	event, err := PullRequestEventFromJSON(ioutil.NopCloser(bytes.NewBuffer(buf)))
-	if err == nil && event.PRNumber != 0 {
+	event := PullRequestEventFromJSON(ioutil.NopCloser(bytes.NewBuffer(buf)))
+	if event != nil && event.PRNumber != 0 {
 		mlog.Info("pr event", mlog.Int("pr", event.PRNumber), mlog.String("action", event.Action))
 		s.handlePullRequestEvent(ctx, event)
 		return