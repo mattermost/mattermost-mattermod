2b29ffd3f02aac0fda55f838a17e721592f72b61
Mario de Frutos Dieguez <mario@defrutos.org>
2020-08-03 19:08:26 +0200

    [MM-27399] Don't break for iteration if one PR fails (#213)
    We don't want to stop checking PR activity when one of the PRs fails for some reason

--- server/pull_request_test.go
+++ server/pull_request_test.go
@@ -1,16 +1,18 @@
-package server_test
+package server
 
 import (
 	"context"
+	"errors"
 	"reflect"
 	"testing"
+	"time"
 
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-github/v32/github"
 
 	"github.com/mattermost/mattermost-mattermod/model"
-	"github.com/mattermost/mattermost-mattermod/server"
-	"github.com/mattermost/mattermost-mattermod/server/mocks"
+	srvmock "github.com/mattermost/mattermost-mattermod/server/mocks"
+	stmock "github.com/mattermost/mattermost-mattermod/store/mocks"
 )
 
 func TestCleanUpLabels(t *testing.T) {
@@ -23,12 +25,12 @@ func TestCleanUpLabels(t *testing.T) {
 	ctxInterface := reflect.TypeOf((*context.Context)(nil)).Elem()
 
 	for name, test := range map[string]struct {
-		SetupClient func(*gomock.Controller) *server.GithubClient
+		SetupClient func(*gomock.Controller) *GithubClient
 	}{
 		"no label has to be removed": {
-			SetupClient: func(ctrl *gomock.Controller) *server.GithubClient {
-				issueMocks := mocks.NewMockIssuesService(ctrl)
-				client := &server.GithubClient{
+			SetupClient: func(ctrl *gomock.Controller) *GithubClient {
+				issueMocks := srvmock.NewMockIssuesService(ctrl)
+				client := &GithubClient{
 					Issues: issueMocks,
 				}
 
@@ -46,9 +48,9 @@ func TestCleanUpLabels(t *testing.T) {
 			},
 		},
 		"all labels have to be removed": {
-			SetupClient: func(ctrl *gomock.Controller) *server.GithubClient {
-				issueMocks := mocks.NewMockIssuesService(ctrl)
-				client := &server.GithubClient{
+			SetupClient: func(ctrl *gomock.Controller) *GithubClient {
+				issueMocks := srvmock.NewMockIssuesService(ctrl)
+				client := &GithubClient{
 					Issues: issueMocks,
 				}
 
@@ -69,9 +71,9 @@ func TestCleanUpLabels(t *testing.T) {
 			},
 		},
 		"some labels have to be removed": {
-			SetupClient: func(ctrl *gomock.Controller) *server.GithubClient {
-				issueMocks := mocks.NewMockIssuesService(ctrl)
-				client := &server.GithubClient{
+			SetupClient: func(ctrl *gomock.Controller) *GithubClient {
+				issueMocks := srvmock.NewMockIssuesService(ctrl)
+				client := &GithubClient{
 					Issues: issueMocks,
 				}
 
@@ -98,8 +100,8 @@ func TestCleanUpLabels(t *testing.T) {
 
 			defer ctrl.Finish()
 
-			s := &server.Server{
-				Config: &server.Config{
+			s := &Server{
+				Config: &Config{
 					IssueLabelsToCleanUp: []string{"AutoMerge", "Do Not Merge", "Work In Progress"},
 				},
 				GithubClient: test.SetupClient(ctrl),
@@ -107,4 +109,195 @@ func TestCleanUpLabels(t *testing.T) {
 			s.CleanUpLabels(pr)
 		})
 	}
+}
+
+func TestCheckPRActivity(t *testing.T) {
+	pr1 := &model.PullRequest{
+		RepoOwner: "owner",
+		RepoName:  "repoName",
+		Number:    123,
+	}
+	pr2 := &model.PullRequest{
+		RepoOwner: "owner",
+		RepoName:  "repoName",
+		Number:    456,
+	}
+	updatedAt := time.Now()
+	ghPR := &github.PullRequest{
+		State:     github.String("open"),
+		UpdatedAt: &updatedAt,
+	}
+	ghPR2 := &github.PullRequest{
+		State:     github.String("open"),
+		UpdatedAt: &updatedAt,
+	}
+	ctrl := gomock.NewController(t)
+	ctxInterface := reflect.TypeOf((*context.Context)(nil)).Elem()
+	defer ctrl.Finish()
+	prStoreMock := stmock.NewMockPullRequestStore(ctrl)
+	prStoreMock.EXPECT().ListOpen().AnyTimes().Return([]*model.PullRequest{pr1, pr2}, nil)
+	ss := stmock.NewMockStore(ctrl)
+	ss.EXPECT().
+		PullRequest().
+		Return(prStoreMock).
+		AnyTimes()
+	metricsMock := srvmock.NewMockMetricsProvider(ctrl)
+	metricsMock.EXPECT().ObserveCronTaskDuration(gomock.Any(), gomock.Any()).AnyTimes()
+	metricsMock.EXPECT().IncreaseCronTaskErrors(gomock.Any()).AnyTimes()
+	cfg := &Config{
+		DaysUntilStale: 1,
+	}
+
+	t.Run("should check for activity but not mark PRs as stale", func(t *testing.T) {
+		prServiceMock := srvmock.NewMockPullRequestsService(ctrl)
+		issuesServiceMock := srvmock.NewMockIssuesService(ctrl)
+		client := &GithubClient{
+			PullRequests: prServiceMock,
+			Issues:       issuesServiceMock,
+		}
+		s := &Server{
+			GithubClient: client,
+			Config:       cfg,
+			Store:        ss,
+			Metrics:      metricsMock,
+		}
+		prServiceMock.EXPECT().Get(gomock.AssignableToTypeOf(ctxInterface),
+			"owner",
+			"repoName",
+			123).Times(1).Return(ghPR, nil, nil)
+		prServiceMock.EXPECT().Get(gomock.AssignableToTypeOf(ctxInterface),
+			"owner",
+			"repoName",
+			456).Times(1).Return(ghPR2, nil, nil)
+		issuesServiceMock.EXPECT().AddLabelsToIssue(gomock.AssignableToTypeOf(ctxInterface),
+			gomock.Any(),
+			gomock.Any(),
+			gomock.Any(),
+			gomock.Any()).Times(0)
+		s.CheckPRActivity()
+	})
+
+	t.Run("should continue with the other PRs if one of them fails", func(t *testing.T) {
+		prServiceMock := srvmock.NewMockPullRequestsService(ctrl)
+		issuesServiceMock := srvmock.NewMockIssuesService(ctrl)
+		client := &GithubClient{
+			PullRequests: prServiceMock,
+			Issues:       issuesServiceMock,
+		}
+		s := &Server{
+			GithubClient: client,
+			Config:       cfg,
+			Store:        ss,
+			Metrics:      metricsMock,
+		}
+		prServiceMock.EXPECT().Get(gomock.AssignableToTypeOf(ctxInterface),
+			"owner",
+			"repoName",
+			123).Times(1).Return(nil, nil, errors.New("fails"))
+		prServiceMock.EXPECT().Get(gomock.AssignableToTypeOf(ctxInterface),
+			"owner",
+			"repoName",
+			456).Times(1).Return(ghPR2, nil, nil)
+		issuesServiceMock.EXPECT().AddLabelsToIssue(gomock.AssignableToTypeOf(ctxInterface),
+			gomock.Any(),
+			gomock.Any(),
+			gomock.Any(),
+			gomock.Any()).Times(0)
+		s.CheckPRActivity()
+	})
+
+	t.Run("should set the stale label ever if one PR fails", func(t *testing.T) {
+		prServiceMock := srvmock.NewMockPullRequestsService(ctrl)
+		issuesServiceMock := srvmock.NewMockIssuesService(ctrl)
+		client := &GithubClient{
+			PullRequests: prServiceMock,
+			Issues:       issuesServiceMock,
+		}
+		cfg := &Config{
+			DaysUntilStale: 0,
+		}
+		s := &Server{
+			GithubClient: client,
+			Config:       cfg,
+			Store:        ss,
+			Metrics:      metricsMock,
+		}
+		prServiceMock.EXPECT().Get(gomock.AssignableToTypeOf(ctxInterface),
+			"owner",
+			"repoName",
+			123).Times(1).Return(ghPR, nil, nil)
+		prServiceMock.EXPECT().Get(gomock.AssignableToTypeOf(ctxInterface),
+			"owner",
+			"repoName",
+			456).Times(1).Return(ghPR2, nil, nil)
+		issuesServiceMock.EXPECT().ListLabelsByIssue(gomock.AssignableToTypeOf(ctxInterface),
+			gomock.Any(),
+			gomock.Any(),
+			123,
+			nil).Times(1).Return(nil, nil, errors.New("fails"))
+		issuesServiceMock.EXPECT().ListLabelsByIssue(gomock.AssignableToTypeOf(ctxInterface),
+			gomock.Any(),
+			gomock.Any(),
+			456,
+			nil).Times(1).Return([]*github.Label{}, nil, nil)
+		issuesServiceMock.EXPECT().AddLabelsToIssue(gomock.AssignableToTypeOf(ctxInterface),
+			gomock.Any(),
+			gomock.Any(),
+			456,
+			gomock.Any()).Times(1)
+		issuesServiceMock.EXPECT().CreateComment(gomock.AssignableToTypeOf(ctxInterface),
+			"owner",
+			"repoName",
+			456,
+			gomock.Any()).Times(1)
+		s.CheckPRActivity()
+	})
+
+	t.Run("should not mark as stale if the exempt stale label is set", func(t *testing.T) {
+		prServiceMock := srvmock.NewMockPullRequestsService(ctrl)
+		issuesServiceMock := srvmock.NewMockIssuesService(ctrl)
+		client := &GithubClient{
+			PullRequests: prServiceMock,
+			Issues:       issuesServiceMock,
+		}
+		cfg := &Config{
+			DaysUntilStale:    0,
+			ExemptStaleLabels: []string{"nostale"},
+		}
+		s := &Server{
+			GithubClient: client,
+			Config:       cfg,
+			Store:        ss,
+			Metrics:      metricsMock,
+		}
+		prServiceMock.EXPECT().Get(gomock.AssignableToTypeOf(ctxInterface),
+			"owner",
+			"repoName",
+			123).Times(1).Return(ghPR, nil, nil)
+		prServiceMock.EXPECT().Get(gomock.AssignableToTypeOf(ctxInterface),
+			"owner",
+			"repoName",
+			456).Times(1).Return(ghPR2, nil, nil)
+		issuesServiceMock.EXPECT().ListLabelsByIssue(gomock.AssignableToTypeOf(ctxInterface),
+			gomock.Any(),
+			gomock.Any(),
+			123,
+			nil).Times(1).Return([]*github.Label{}, nil, nil)
+		issuesServiceMock.EXPECT().ListLabelsByIssue(gomock.AssignableToTypeOf(ctxInterface),
+			gomock.Any(),
+			gomock.Any(),
+			456,
+			nil).Times(1).Return([]*github.Label{{Name: github.String("nostale")}}, nil, nil)
+		issuesServiceMock.EXPECT().AddLabelsToIssue(gomock.AssignableToTypeOf(ctxInterface),
+			gomock.Any(),
+			gomock.Any(),
+			123,
+			gomock.Any()).Times(1)
+		issuesServiceMock.EXPECT().CreateComment(gomock.AssignableToTypeOf(ctxInterface),
+			"owner",
+			"repoName",
+			123,
+			gomock.Any()).Times(1)
+		s.CheckPRActivity()
+	})
 }
