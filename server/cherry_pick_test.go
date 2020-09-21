f9d7fc69d3248b75f9ca2568b6871eb543ee8750
Agniva De Sarker <agnivade@yahoo.co.in>
2020-09-21 10:28:00 +0530

    MM-28886: Fix panic on out-of-bounds error
    We check if a release string is present or not before continuing
    with the cherry pick request.
    
    https://mattermost.atlassian.net/browse/MM-28886

--- server/cherry_pick_test.go
+++ server/cherry_pick_test.go
@@ -81,6 +81,17 @@ func TestHandleCherryPick(t *testing.T) {
 		err = s.handleCherryPick(context.Background(), "org-member", "/cherry-pick release-5.28", pr)
 		require.EqualError(t, err, "too many requests")
 	})
+
+	t.Run("should not panic on empty requests", func(t *testing.T) {
+		defer func() {
+			if r := recover(); r != nil {
+				require.Failf(t, "recovered from panic", "%v", r)
+			}
+		}()
+
+		err := s.handleCherryPick(context.Background(), "org-member", "/cherry-pick", pr)
+		require.NoError(t, err)
+	})
 }
 
 func TestGetMilestone(t *testing.T) {
