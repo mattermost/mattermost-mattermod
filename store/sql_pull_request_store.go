b5cc96ef4d1bb67b38557f340360774a52e38c53
Rodrigo Villablanca <villa061004@gmail.com>
2020-09-24 00:29:16 -0300

    First approach

--- store/sql_pull_request_store.go
+++ store/sql_pull_request_store.go
@@ -6,43 +6,40 @@ package store
 import (
 	"database/sql"
 	"fmt"
+	"strings"
 
 	"github.com/mattermost/mattermost-mattermod/model"
 )
 
 type SQLPullRequestStore struct {
 	*SQLStore
+	insertQuery string
+	updateQuery string
 }
 
 func NewSQLPullRequestStore(sqlStore *SQLStore) PullRequestStore {
-	s := &SQLPullRequestStore{sqlStore}
+	s := &SQLPullRequestStore{SQLStore: sqlStore}
+	var b strings.Builder
+	b.WriteString("INSERT INTO PullRequests ")
+	b.WriteString("(RepoOwner, RepoName, FullName, Number, Username, Ref, Sha, Labels, State, BuildStatus, ")
+	b.WriteString("BuildConclusion, BuildLink, URL, CreatedAt, Merged, MaintainerCanModify) ")
+	b.WriteString("VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16)")
+	s.insertQuery = b.String()
 
-	// for _, db := range sqlStore.GetAllConns() {
-	// 	table := db.AddTableWithName(model.PullRequest{}, "PullRequests").SetKeys(false, "RepoOwner", "RepoName", "Number")
-	// 	table.ColMap("RepoOwner").SetMaxSize(128)
-	// 	table.ColMap("RepoName").SetMaxSize(128)
-	// 	table.ColMap("Username").SetMaxSize(128)
-	// 	table.ColMap("FullName").SetMaxSize(2083)
-	// 	table.ColMap("Ref").SetMaxSize(128)
-	// 	table.ColMap("Sha").SetMaxSize(48)
-	// 	table.ColMap("State").SetMaxSize(8)
-	// 	table.ColMap("Labels").SetMaxSize(1024)
-	// 	table.ColMap("BuildStatus").SetMaxSize(8)
-	// 	table.ColMap("BuildLink").SetMaxSize(256)
-	// 	table.ColMap("BuildConclusion").SetMaxSize(256)
-	// 	table.ColMap("URL").SetMaxSize(2083)
-	// 	table.ColMap("CreatedAt").SetMaxSize(128)
-	// 	table.ColMap("MaintainerCanModify")
-	// 	table.ColMap("Merged")
-	// }
+	b.Reset()
+	b.WriteString("UPDATE PullRequests SET FullName = $1, Username = $2, Ref = $3, Sha = $4, Labels = $5, ")
+	b.WriteString("State = $6, BuildStatus = $7, BuildConclusion = $8, BuildLink = $9, URL = $10, CreatedAt = $11, Merged = $12, ")
+	b.WriteString("MaintainerCanModify = $13 WHERE RepoOwner = $14 AND RepoName = $15 AND Number = $16")
+	s.updateQuery = b.String()
 
 	return s
 }
 
 func (s SQLPullRequestStore) Save(pr *model.PullRequest) (*model.PullRequest, error) {
-	// s.dbx.Exec(`INSERT INTO PullRequests (a,b,c) VALUES ($1, $2, $2)`, field1, field2, field3)
-	if err := s.GetMaster().Insert(pr); err != nil {
-		if _, err := s.GetMaster().Update(pr); err != nil {
+	if _, err := s.dbx.Exec(s.insertQuery, pr.RepoOwner, pr.RepoName, pr.FullName, pr.Number, pr.Username, pr.Ref, pr.Sha, pr.Labels, pr.State,
+		pr.BuildStatus, pr.BuildConclusion, pr.BuildLink, pr.URL, pr.CreatedAt, pr.Merged, pr.MaintainerCanModify); err != nil {
+		if _, err := s.dbx.Exec(s.updateQuery, pr.FullName, pr.Username, pr.Ref, pr.Sha, pr.Labels, pr.State, pr.BuildStatus, pr.BuildConclusion,
+			pr.BuildLink, pr.URL, pr.CreatedAt, pr.Merged, pr.MaintainerCanModify, pr.RepoOwner, pr.RepoName, pr.Number); err != nil {
 			return nil, fmt.Errorf("could not insert or update PR: owner=%v, name=%v, number=%v, err=%w", pr.RepoOwner, pr.RepoName, pr.Number, err)
 		}
 	}
