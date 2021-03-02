de8059be310f3401284e276c73901086ac32ccb0
Elisabeth Kulzer <elikul@elikul.de>
2021-03-02 09:55:13 +0100

    Test ee (#255)

--- server/enterprise.go
+++ server/enterprise.go
@@ -36,9 +36,9 @@ func (s *Server) triggerEnterpriseTests(pr *model.PullRequest) {
 		s.createEnterpriseTestsErrorStatus(ctx, pr, err)
 		return
 	}
-	if triggerInfo.BaseBranch != "master" && triggerInfo.BaseBranch != "cloud-ga" && !isBaseBranchReleaseBranch {
+	if triggerInfo.BaseBranch != "master" && triggerInfo.BaseBranch != "cloud" && triggerInfo.BaseBranch != "cloud-ga" && !isBaseBranchReleaseBranch {
 		mlog.Debug("Succeeding ee statuses", mlog.Int("pr", pr.Number), mlog.String("base ref", triggerInfo.BaseBranch))
-		s.succeedEEStatuses(ctx, pr, "base branch not master or release or cloud-ga")
+		s.succeedEEStatuses(ctx, pr, "base branch not master or release or cloud or cloud-ga")
 		return
 	}
 
