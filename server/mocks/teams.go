bce8dda11d90b3e703a238681a609bd14bb8a0bb
Ibrahim Serdar Acikgoz <serdaracikgoz86@gmail.com>
2021-03-30 12:12:39 +0300

    update dependencies

--- server/mocks/teams.go
+++ server/mocks/teams.go
@@ -7,7 +7,7 @@ package mocks
 import (
 	context "context"
 	gomock "github.com/golang/mock/gomock"
-	github "github.com/google/go-github/v32/github"
+	github "github.com/google/go-github/v33/github"
 	reflect "reflect"
 )
 
