d8cee8cdea08400f3da2a1031d76f148d0d967e3
Mario Vitale <mario.vitale@flixbus.com>
2022-03-31 14:50:28 +0200

    Add the negative test

--- server/e2e_test_qa_workflow_test.go
+++ server/e2e_test_qa_workflow_test.go
@@ -66,7 +66,7 @@ func TestE2EQAWorkflow(t *testing.T) {
         ts := httptest.NewServer(http.HandlerFunc(s.pullRequestEventHandler))
         defer ts.Close()
 
-        t.Run("Event should trigger E2E test", func(t *testing.T) {
+        t.Run("Event has correct label, should trigger E2E test", func(t *testing.T) {
 		eventAction := "labeled"
 		event := pullRequestEvent{
 		        Action: eventAction,
@@ -161,6 +161,108 @@ func TestE2EQAWorkflow(t *testing.T) {
                 is.EXPECT().
 			CreateComment(gomock.AssignableToTypeOf(ctxInterface), "mattertest", "mattermod", 1, comment).
 			Times(1).
+			Return(nil, nil, nil)
+
+                req, err := http.NewRequest("POST", ts.URL, bytes.NewReader(b))
+                require.NoError(t, err)
+                resp, err := http.DefaultClient.Do(req)
+                require.NoError(t, err)
+                defer resp.Body.Close()
+                require.Equal(t, http.StatusOK, resp.StatusCode)
+        })
+        t.Run("Event has the wrong label, should not trigger E2E test", func(t *testing.T) {
+		eventAction := "labeled"
+		eventLabel := "NotTheQAOne"
+		event := pullRequestEvent{
+		        Action: eventAction,
+			Label: &github.Label{
+				Name: &eventLabel,
+			},
+		        PRNumber: 1,
+		        PullRequest: &github.PullRequest{
+		                Number: github.Int(1),
+		                Base: &github.PullRequestBranch{
+		                        Repo: &github.Repository{
+		                                Owner: &github.User{
+		                                        Login: github.String("mattertest"),
+		                                },
+		                                Name: github.String("mattermod"),
+		                        },
+		                },
+		                Head: &github.PullRequestBranch{
+		                        SHA: github.String("sha"),
+		                },
+		        },
+			Sender: &pullRequestEventSender{
+				Login: "ghUser",
+			},
+		}
+
+                rs.EXPECT().
+                        GetCombinedStatus(gomock.AssignableToTypeOf(ctxInterface), "mattertest", "mattermod", "sha", nil).
+			AnyTimes().
+                        Return(&github.CombinedStatus{
+                                Statuses: []*github.RepoStatus{
+                                        {
+                                                Context: github.String("something"),
+                                        },
+                                },
+                        }, nil, nil)
+
+                cs.EXPECT().
+                        ListCheckRunsForRef(gomock.AssignableToTypeOf(ctxInterface), "mattertest", "mattermod", "sha", nil).
+                        Return(&github.ListCheckRunsResults{}, nil, nil)
+
+                is.EXPECT().
+                        ListLabelsByIssue(gomock.AssignableToTypeOf(ctxInterface), "mattertest", "mattermod", 1, nil).
+                        Return([]*github.Label{}, nil, nil)
+
+		prState := "open"
+		prMergeableState := "clean"
+		prApprovalState := "approved"
+		prApprovalReviews := []*github.PullRequestReview{
+			&github.PullRequestReview{
+                                State: &prApprovalState,
+                        },
+		}
+		prGhModel := github.PullRequest{
+			Labels: []*github.Label{event.Label},
+			State: &prState,
+			MergeableState: &prMergeableState,
+		}
+		prs.EXPECT().
+			ListReviews(gomock.AssignableToTypeOf(ctxInterface), "mattertest", "mattermod", 1, nil).
+			Return(prApprovalReviews, nil, nil)
+		prs.EXPECT().
+			Get(gomock.AssignableToTypeOf(ctxInterface), "mattertest", "mattermod", 1).
+			Return(&prGhModel, nil, nil)
+
+                prStoreMock.EXPECT().Save(gomock.AssignableToTypeOf(&model.PullRequest{})).
+                        Return(nil, nil)
+
+                prStoreMock.EXPECT().Get("mattertest", "mattermod", 1).
+                        Return(&model.PullRequest{
+                        RepoOwner:           "mattertest",
+                        RepoName:            "mattermod",
+                        CreatedAt:           time.Time{},
+                        Labels:              []string{"old-label"},
+                        Sha:                 "sha",
+                        MaintainerCanModify: NewBool(false),
+                        Merged:              NewBool(false),
+                }, nil)
+
+                prStoreMock.EXPECT().Save(gomock.AssignableToTypeOf(&model.PullRequest{})).
+                        Return(nil, nil)
+
+                b, err := json.Marshal(event)
+                require.NoError(t, err)
+
+		s.OrgMembers = []string{}
+		msg := e2eTestMsgCommenterPermission
+		comment := &github.IssueComment{Body: &msg}
+                is.EXPECT().
+			CreateComment(gomock.AssignableToTypeOf(ctxInterface), "mattertest", "mattermod", 1, comment).
+			Times(0).
 			Return(nil, nil, nil)
 
                 req, err := http.NewRequest("POST", ts.URL, bytes.NewReader(b))
